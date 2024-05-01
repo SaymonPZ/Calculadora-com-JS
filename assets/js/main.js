880788e349fa692000a1f51e52d46a63e721f47d
Saymon <saymon.espindola@gmail.com>
2024-05-01 19:24:37 -0300

    regex validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,10 @@
 function makeCalculator() {
     return {
         display: document.querySelector('.display'),
-        displayAcumulator: document.querySelector('#displayAcumulator'),
-        acumulator: 0,
-        operatorAcumulator: "",
 
         start() {
             this.btnClick();
+            this.enterButon();
         },
         btnClick() {
             document.addEventListener('click', (e) => {
@@ -19,25 +17,46 @@ function makeCalculator() {
                     console.log('clear display');
                 } else if (element.classList.contains('btnBack')) {
                     this.displayBackspace();
-                } else if (element.classList.contains('btnOper')){
-                    this.operatorAcumulator = element.innerText;
+                } else if (element.classList.contains('btnEqui')){
+                    this.makeAccount();
+                }
+            });
+        },
+        enterButon(){
+            this.display.addEventListener('keyup', (e) => {
+                console.log(e.keycode);
+                if(e.keyCode === 13){
                     this.makeAccount();
                 }
             });
         },
         btnDrawDisplay(num) {
-            this.display.value += num;
+            const validChars = /[0-9()+\-*/.]+/;
+            if (validChars.test(num)) {
+                this.display.value += num;
+                this.display.focus();
+            } else {
+                console.log('Invalid input');
+            }
         },
         clearDisplay(){
             this.display.value = '';
-            this.displayAcumulator.innerText = '';
-            this.acumulator = 0;
         },
         displayBackspace(){
             this.display.value = this.display.value.slice(0, -1);
         },
         makeAccount(){
-
+            try {
+                const expression = this.display.value.trim();
+                const validExpression = /^[0-9()+\-*/.]+$/;
+                if (validExpression.test(expression)) {
+                    this.display.value = eval(expression);
+                } else {
+                    this.display.value = 'Error';
+                }
+            } catch (e) {
+                this.display.value = 'Error';
+            }
         },
     };
 }
