46180ca49f616fc6a52208be79e1357381e1b4bd
Saymon <saymon.espindola@gmail.com>
2024-04-30 23:47:47 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,44 @@
-function makeCalculator(){
-    return{
+function makeCalculator() {
+    return {
         display: document.querySelector('.display'),
-        btns: document.querySelector('.btn'),
+        displayAcumulator: document.querySelector('#displayAcumulator'),
+        acumulator: 0,
+        operatorAcumulator: "",
 
-        start(){
+        start() {
             this.btnClick();
         },
-        btnClick(){
-            document.addEventListener('click', (e)=>{
+        btnClick() {
+            document.addEventListener('click', (e) => {
                 const element = e.target;
 
-                if(element.classList.contains('btn-num')){
+                if (element.classList.contains('btn-num')) {
                     this.btnDrawDisplay(element.innerText);
+                } else if (element.classList.contains('btnClear')) {
+                    this.clearDisplay();
+                    console.log('clear display');
+                } else if (element.classList.contains('btnBack')) {
+                    this.displayBackspace();
+                } else if (element.classList.contains('btnOper')){
+                    this.operatorAcumulator = element.innerText;
+                    this.makeAccount();
                 }
             });
         },
-        btnDrawDisplay(num){
+        btnDrawDisplay(num) {
             this.display.value += num;
-        }
+        },
+        clearDisplay(){
+            this.display.value = '';
+            this.displayAcumulator.innerText = '';
+            this.acumulator = 0;
+        },
+        displayBackspace(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+        makeAccount(){
+
+        },
     };
 }
 
